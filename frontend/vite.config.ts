24b5cd762bc089c8b19f30bffa41774618a74b67
Alexandre Shinebourne <a.shinebourne@gmail.com>
2022-09-26 22:10:17 +0100

    working pwa config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Oolong',
+        description: 'A life companion',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/192-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/512-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ]
 })
