1ff49b18d995c5d45a2746cfe9ed349728786977
Alexandre Shinebourne <a.shinebourne@gmail.com>
2022-09-30 16:39:24 +0100

    added online offline state

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,26 +1,43 @@
 <script lang="ts">
-  import { ConnectionStatus } from "svelte-connection-status";
+  import { onMount } from "svelte";
 
-  function handleChange({ detail }) {
-    if (detail.isOffline) {
-      console.log(">>> you are offline");
-    }
+  let onlineFlag: boolean;
+  window.addEventListener("offline", (e) => {
+    console.log("offline");
+    onlineFlag = false;
+  });
+  window.addEventListener("online", (e) => {
+    console.log("online");
+    onlineFlag = true;
+  });
 
-    if (detail.isOnline) {
-      console.log(">>> you are online");
+  const dbrequest = window.indexedDB.open("todos");
+  dbrequest.onerror = (e) => {
+    console.log("Unable to open a connetion to the local db");
+  };
+  dbrequest.onsuccess = (e) => {
+    console.log("Yay connected");
+    const db = e.target.result;
+    // console.log(db);
+    const taskStore = db.createObjectStore("tasks", { keyPath: "ssn" });
+  };
+
+  onMount(() => {
+    if (navigator.onLine) {
+      onlineFlag = true;
+    } else {
+      onlineFlag = false;
     }
-  }
+  });
 </script>
 
 <main>
+  {#if onlineFlag}
+    <span>online</span>
+  {:else}
+    <span>offline</span>
+  {/if}
   <h1>Hello world!</h1>
-  <p>
-    you are
-    <ConnectionStatus on:change={handleChange}>
-      <span slot="offline">offline :(</span>
-      <span slot="online">online :)</span>
-    </ConnectionStatus>
-  </p>
 </main>
 
 <style>
