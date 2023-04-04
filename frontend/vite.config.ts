7312fed9127314efddda772d4ef4ef34068f6d98
Alexandre Shinebourne <a.shinebourne@gmail.com>
2023-04-04 11:55:38 +0100

    modifying pwa background

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,15 +8,6 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/oolong/",
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
-      },
-    },
-  },
   plugins: [
     svelte(),
     VitePWA({
