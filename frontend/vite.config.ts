6e500de7f9d58956a2231e1b30c5f1c1591d49aa
Alexandre Shinebourne <alexandre@shinebourne.com>
2023-05-08 15:44:00 +0100

    updating now using custom domain

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import manifest from "./manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/oolong/", // for github pages to fetch assets
+  // base: "/oolong/", // for github pages to fetch assets
   define: { global: "window" },
   plugins: [
     svelte(),
